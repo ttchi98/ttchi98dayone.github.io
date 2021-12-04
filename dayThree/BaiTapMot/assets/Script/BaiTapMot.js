991e664450ddcb1b6574b70f4e9d7219685a520a
ttchi98 <ttchi.29041998@gmail.com>
2021-12-04 11:58:23 +0700

    update  BaiTapMot

---
+++ dayThree/BaiTapMot/assets/Script/BaiTapMot.js
@@ -0,0 +1,19 @@
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    userNameEditBox: cc.EditBox,
+    passWordEditBox: cc.EditBox,
+    emailEditBox: cc.EditBox,
+    phoneNumberEditBox: cc.EditBox,
+  },
+
+  // LIFE-CYCLE CALLBACKS:
+
+  onLoad() {},
+
+  start() {},
+
+  validate() {},
+  update(dt) {},
+});
