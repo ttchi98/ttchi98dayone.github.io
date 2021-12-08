9d192b7260683f8e9b6c14d05768c7533df81104
ttchi98 <ttchi.29041998@gmail.com>
2021-12-08 17:41:01 +0700

    update Final Project -v2

--- Final Project/finalProject-2048/assets/Script/HelloWorld.js
+++ Final Project/finalProject-2048/assets/Script/HelloWorld.js
@@ -1,22 +1,17 @@
 cc.Class({
-    extends: cc.Component,
+  extends: cc.Component,
 
-    properties: {
-        label: {
-            default: null,
-            type: cc.Label
-        },
-        // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
+  properties: {
+    label: {
+      default: null,
+      type: cc.Label,
     },
+    // defaults, set visually when attaching this script to the Canvas
+  },
 
-    // use this for initialization
-    onLoad: function () {
-        this.label.string = this.text;
-    },
+  // use this for initialization
+  onLoad: function () {},
 
-    // called every frame
-    update: function (dt) {
-
-    },
+  // called every frame
+  update: function (dt) {},
 });
